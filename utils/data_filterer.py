8f92c4b04053e529973dfc1c1da59f00a8b762e2
vjz3qz <vjz3qz@virginia.edu>
2023-08-02 19:32:28 -0400

    Added the filter selection to data filterer

--- utils/data_filterer.py
+++ utils/data_filterer.py
@@ -4,8 +4,13 @@ import pandas as pd
 class DataFilterer:
     def __init__(self, df):
         self.data_frame = df
+        self.filter_methods = {
+            1: self.filter_apps_with_restricted_ppi_or_cash_payment_systems,
+            2: self.filter_apps_with_restricted_ppi_or_cash_payment_systems,
+            3: self.filter_apps_with_restricted_ppi_or_cash_payment_systems,
+            4: self.filter_apps_with_restricted_ppi_or_cash_payment_systems
+        }
 
-    # Encapsulated the filter method inside the class
     def filter_apps_with_restricted_ppi_or_cash_payment_systems(self):
         self.data_frame = self.data_frame[
             (self.data_frame['Deployment_Lifecycle_Phase'] == 'Production') &
@@ -17,3 +22,10 @@ class DataFilterer:
             (self.data_frame['NFR_10'] != 'N/A')
             ]
 
+    # Define other filter methods here
+
+    def apply_filter(self, filter_option):
+        if filter_option in self.filter_methods:
+            self.filter_methods[filter_option]()
+        else:
+            raise ValueError(f'Invalid filter option: {filter_option}')
