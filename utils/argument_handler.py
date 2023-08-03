472808b4eb609a8642f39822c9bb8399dcd595a7
vjz3qz <vjz3qz@virginia.edu>
2023-08-02 20:14:53 -0400

    Added error handling and updated readme

--- utils/argument_handler.py
+++ utils/argument_handler.py
@@ -7,13 +7,20 @@ class ArgumentHandler:
         print("3. NFR")
         print("4. All")
         while True:
-            user_input = int(input("Enter your choice (1-4): "))
-            if 1 <= user_input <= 4:
-                return user_input
-            else:
-                print("Invalid choice. Please try again.")
+            try:
+                user_input = int(input("Enter your choice (1-4): "))
+                if 1 <= user_input <= 4:
+                    return user_input
+                else:
+                    print("Invalid choice. Please try again.")
+            except ValueError:
+                print("Error: Please enter a number.")
 
     @staticmethod
     def get_sheet_name():
-        sheet_name = input("Enter the name of the sheet to read from: ")
-        return sheet_name
+        while True:
+            sheet_name = input("Enter the name of the sheet to read from: ")
+            if sheet_name:  # If the string is not empty
+                return sheet_name
+            else:
+                print("Error: Sheet name cannot be empty.")
--- utils/data_filterer.py
+++ utils/data_filterer.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 class DataFilterer:
     def __init__(self, df):
+        if not isinstance(df, pd.DataFrame):
+            raise ValueError("Expected pandas DataFrame, got: {}".format(type(df)))
         self.data_frame = df
         self.filter_methods = {
             1: self.filter_apps_with_restricted_ppi_or_cash_payment_systems,
@@ -12,20 +14,27 @@ class DataFilterer:
         }
 
     def filter_apps_with_restricted_ppi_or_cash_payment_systems(self):
-        self.data_frame = self.data_frame[
-            (self.data_frame['Deployment_Lifecycle_Phase'] == 'Production') &
-            ((self.data_frame['Asset_Type'] == 'Application') |
-             (self.data_frame['Asset_Type'] == 'Platform') |
-             (self.data_frame['Asset_Type'] == 'Tool')) &
-            ((self.data_frame['PPI_Classification'] == 'Restricted PPI') |
-             (self.data_frame['Cash_Payment_systems'] == 'Yes')) &
-            (self.data_frame['NFR_10'] != 'N/A')
+        try:
+            self.data_frame = self.data_frame[
+                (self.data_frame['Deployment_Lifecycle_Phase'] == 'Production') &
+                ((self.data_frame['Asset_Type'] == 'Application') |
+                (self.data_frame['Asset_Type'] == 'Platform') |
+                (self.data_frame['Asset_Type'] == 'Tool')) &
+                ((self.data_frame['PPI_Classification'] == 'Restricted PPI') |
+                (self.data_frame['Cash_Payment_systems'] == 'Yes')) &
+                (self.data_frame['NFR_10'] != 'N/A')
             ]
+        except KeyError as e:
+            raise KeyError(f"DataFrame does not have necessary columns for filtering: {str(e)}")
+        except Exception as e:
+            raise Exception(f"An unexpected error occurred during data filtering: {str(e)}")
 
     # Define other filter methods here
 
     def apply_filter(self, filter_option):
-        if filter_option in self.filter_methods:
-            self.filter_methods[filter_option]()
-        else:
+        if filter_option not in self.filter_methods:
             raise ValueError(f'Invalid filter option: {filter_option}')
+        try:
+            self.filter_methods[filter_option]()
+        except Exception as e:
+            raise Exception(f"Failed to apply filter: {str(e)}")
